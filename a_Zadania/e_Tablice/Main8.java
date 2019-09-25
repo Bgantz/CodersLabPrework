17d5be2cd4c98c4c29fc51bf9bbb67f8b995f8e4
Blazej Gantz <blazej.gantz@gmail.com>
2019-09-25 22:47:48 +0200

    final?

--- a_Zadania/e_Tablice/Main8.java
+++ a_Zadania/e_Tablice/Main8.java
@@ -16,6 +16,22 @@ public class Main8 {
 
 	public static void main(String[] args) {
 		double[] temps = {30, 29, 14, 42, -4, -10, 8, 14, 32, 11, 8, 0, -15};
+		double[] tempFahr = {temps.length};
+		double avg;
+
+		for (double tempCelc: temps) {
+			tempFahr.add(tempCelc * 1.8 + 32);
+		}
+
+		int sum = 0;
+		for (double value : tempFahr) {
+			sum += value;
+		}
+		avg = sum / tempFahr.length;
+
+		String.format("%2f", avg);
+		System.out.println("Średnia temperatura to: " + avg);
+
 	}
 
 }
